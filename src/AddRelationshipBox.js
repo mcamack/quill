1a22bf17583574a164b6e6488d0ff3b162e92953
argyle <mcamack@gmail.com>
2024-12-08 21:25:15 -0800

    box mostly done

--- src/AddRelationshipBox.js
+++ src/AddRelationshipBox.js
@@ -5,22 +5,26 @@ import axios from 'axios';
 
 const AddRelationshipBox = () => {
     const [dialogOpen, setDialogOpen] = useState(false);
-    const [sourceNode, setSourceNode] = useState(null);
-    const [targetNode, setTargetNode] = useState(null);
+    const [sourceNodeType, setSourceNodeType] = useState(null);
+    const [sourceNodeName, setSourceNodeName] = useState(null);
+    const [targetNodeType, setTargetNodeType] = useState(null);
+    const [targetNodeName, setTargetNodeName] = useState(null);
     const [relationshipType, setRelationshipType] = useState(null);
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
     const handleOpenDialog = () => setDialogOpen(true);
     const handleCloseDialog = () => {
         setDialogOpen(false);
-        setSourceNode(null);
-        setTargetNode(null);
+        setSourceNodeType(null);
+        setSourceNodeName(null);
+        setTargetNodeType(null);
+        setTargetNodeName(null);
         setRelationshipType(null);
         setIsButtonDisabled(true);
     };
 
     const handleSelectChange = () => {
-        if (sourceNode && targetNode && relationshipType) {
+        if (sourceNodeType && sourceNodeName && targetNodeType && targetNodeName && relationshipType) {
             setIsButtonDisabled(false);
         } else {
             setIsButtonDisabled(true);
@@ -29,10 +33,10 @@ const AddRelationshipBox = () => {
 
     const handleCreateRelationship = async () => {
         const payload = {
-            source_node_label: sourceNode.label,
-            source_node_name: sourceNode.name,
-            target_node_label: targetNode.label,
-            target_node_name: targetNode.name,
+            source_node_label: sourceNodeType.value,
+            source_node_name: sourceNodeName.value,
+            target_node_label: targetNodeType.value,
+            target_node_name: targetNodeName.value,
             relationship_type: relationshipType.value,
             properties: {}, // Add properties as needed
         };
@@ -46,10 +50,58 @@ const AddRelationshipBox = () => {
         }
     };
 
-    const nodeOptions = [
-        { label: 'Node A', name: 'NodeA', value: 'NodeA' },
-        { label: 'Node B', name: 'NodeB', value: 'NodeB' },
-        // Add more node options as needed
+    const sourceNodeOptions = [
+        {
+            label: 'Requirement_Specification', value: 'Requirement_Specification',
+            options2: [
+                { label: 'Spec A1', value: 'Spec A1' },
+                { label: 'Spec A2', value: 'Spec A2' },
+                { label: 'Spec A3', value: 'Spec A3' },
+                { label: 'Spec A4', value: 'Spec A4' },
+                { label: 'Spec A5', value: 'Spec A5' },
+            ],
+        },
+        {
+            label: 'Test_Plan', value: 'Test_Plan',
+            options2: [
+                { label: 'Test Plan B1', value: 'Test Plan B1' },
+                { label: 'Test Plan B2', value: 'Test Plan B2' },
+            ],
+        },
+        {
+            label: 'Activity_Diagram', value: 'Activity_Diagram',
+            options2: [
+                { label: 'Diagram ABC', value: 'Diagram ABC' },
+                { label: 'Diagram ABCD', value: 'Diagram ABCD' },
+            ],
+        },
+    ];
+
+    const targetNodeOptions = [
+        {
+            label: 'Requirement_Specification', value: 'Requirement_Specification',
+            options2: [
+                { label: 'Spec A1', value: 'Spec A1' },
+                { label: 'Spec A2', value: 'Spec A2' },
+                { label: 'Spec A3', value: 'Spec A3' },
+                { label: 'Spec A4', value: 'Spec A4' },
+                { label: 'Spec A5', value: 'Spec A5' },
+            ],
+        },
+        {
+            label: 'Test_Plan', value: 'Test_Plan',
+            options2: [
+                { label: 'Test Plan B1', value: 'Test Plan B1' },
+                { label: 'Test Plan B2', value: 'Test Plan B2' },
+            ],
+        },
+        {
+            label: 'Activity_Diagram', value: 'Activity_Diagram',
+            options2: [
+                { label: 'Diagram ABC', value: 'Diagram ABC' },
+                { label: 'Diagram ABCD', value: 'Diagram ABCD' },
+            ],
+        },
     ];
 
     const relationshipOptions = [
@@ -80,10 +132,6 @@ const AddRelationshipBox = () => {
             maxHeight: '600px',
             overflowY: 'auto',
         }),
-        // control: (provided) => ({
-        //     ...provided,
-        //     border: '2px solid black',
-        // }),
         menuPortal: (provided) => ({
             ...provided,
             zIndex: 9999, // Ensure the menu appears above other elements
@@ -99,50 +147,91 @@ const AddRelationshipBox = () => {
             <Dialog open={dialogOpen} onClose={handleCloseDialog} maxWidth={false}>
                 <div style={{
                     width: '1000px',
-                    // height: '800px' 
                 }}>
                     <DialogTitle>Add Relationship</DialogTitle>
                     <DialogContent>
                         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
-                            <div style={{ width: '270px', position: 'relative', top: '24px' }}>
-                                <label>Source Node</label>
-                                <Select
-                                    options={nodeOptions}
-                                    value={sourceNode}
-                                    onChange={(selected) => {
-                                        setSourceNode(selected);
-                                        handleSelectChange();
-                                    }}
-                                    styles={customNodeSelectStyles}
-                                    menuPortalTarget={document.body}
-                                />
-                            </div>
-                            <div style={{ width: '240px', textAlign: 'center' }}>
+                            <span>
+                                <div style={{ width: '270px', position: 'relative', top: '24px' }}>
+                                    <label>Source Node Type</label>
+                                    <Select
+                                        options={sourceNodeOptions}
+                                        value={sourceNodeType}
+                                        onChange={(selected) => {
+                                            setSourceNodeType(selected);
+                                            setSourceNodeName(null);
+                                            handleSelectChange();
+                                        }}
+                                        styles={customNodeSelectStyles}
+                                        menuPortalTarget={document.body}
+                                    />
+                                </div>
+                                <div style={{ width: '270px', position: 'relative', top: '24px' }}>
+                                    <label>Source Node Name</label>
+                                    <Select
+                                        options={sourceNodeType ? sourceNodeType.options2 : []}
+                                        value={sourceNodeName}
+                                        onChange={(selected) => {
+                                            setSourceNodeName(selected);
+                                            handleSelectChange();
+                                        }}
+                                        styles={customNodeSelectStyles}
+                                        isDisabled={!sourceNodeType}
+                                        menuPortalTarget={document.body}
+                                        placeholder=""
+                                    />
+                                </div>
+                            </span>
+                            <div style={{ width: '240px', textAlign: 'center', position: 'relative', top: '20px' }}>
                                 <label>Relationship Type</label>
                                 <Select
                                     options={relationshipOptions}
                                     value={relationshipType}
                                     onChange={(selected) => {
-                                        setRelationshipType(selected);
-                                        handleSelectChange();
+                                        if (sourceNodeType && sourceNodeName && targetNodeType && targetNodeName) {
+                                            setRelationshipType(selected);
+                                            handleSelectChange();
+                                        }
                                     }}
                                     styles={customRelationshipSelectStyles}
+                                    isDisabled={!sourceNodeType || !sourceNodeName || !targetNodeType || !targetNodeName}
                                     menuPortalTarget={document.body}
+                                    placeholder=""
                                 />
                             </div>
-                            <div style={{ width: '270px', position: 'relative', top: '24px' }}>
-                                <label>Target Node</label>
-                                <Select
-                                    options={nodeOptions}
-                                    value={targetNode}
-                                    onChange={(selected) => {
-                                        setTargetNode(selected);
-                                        handleSelectChange();
-                                    }}
-                                    styles={customNodeSelectStyles}
-                                    menuPortalTarget={document.body}
-                                />
-                            </div>
+                            <span>
+                                <div style={{ width: '270px', position: 'relative', top: '24px' }}>
+                                    <label>Target Node Type</label>
+                                    <Select
+                                        options={targetNodeOptions}
+                                        value={targetNodeType}
+                                        onChange={(selected) => {
+                                            setTargetNodeType(selected);
+                                            setTargetNodeName(null);
+                                            handleSelectChange();
+                                        }}
+                                        styles={customNodeSelectStyles}
+                                        isDisabled={!sourceNodeType}
+                                        menuPortalTarget={document.body}
+                                        placeholder=""
+                                    />
+                                </div>
+                                <div style={{ width: '270px', position: 'relative', top: '24px' }}>
+                                    <label>Target Node Name</label>
+                                    <Select
+                                        options={targetNodeType ? targetNodeType.options2 : []}
+                                        value={targetNodeName}
+                                        onChange={(selected) => {
+                                            setTargetNodeName(selected);
+                                            handleSelectChange();
+                                        }}
+                                        styles={customNodeSelectStyles}
+                                        isDisabled={!targetNodeType}
+                                        menuPortalTarget={document.body}
+                                        placeholder=""
+                                    />
+                                </div>
+                            </span>
                         </div>
                         <div style={{ marginTop: '2rem', top: '-28px', position: 'relative', width: '412px', height: '2px', backgroundColor: 'black', marginLeft: 'auto', marginRight: 'auto' }}>
                             <div style={{
